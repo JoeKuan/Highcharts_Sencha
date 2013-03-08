3a308abad676f7205f445440d81fad4801615341
straccio <ambrosini_c@MacBook-Pro-di-Claudio-Ambrosini.local>
2013-03-08 08:25:59 +0100

    Missing merge

--- Chart/ux/Highcharts/RPieSerie.js
+++ Chart/ux/Highcharts/RPieSerie.js
@@ -933,119 +933,119 @@ Ext.define('Chart.ux.Highcharts.RPieSerie', {
    */
 	columns : [],
 
-	constructor : function(config) {
-		this.callParent(arguments);
-		if(this.useTotals) {
-			this.columnData = {};
-			var length = this.columns.length;
-			for(var i = 0; i < length; i++) {
-				this.columnData[this.columns[i]] = 100 / length;
-			}
-		}
-	},
+    constructor : function(config) {
+        this.callParent(arguments);
+        if(this.useTotals) {
+            this.columnData = {};
+            var length = this.columns.length;
+            for(var i = 0; i < length; i++) {
+                this.columnData[this.columns[i]] = 100 / length;
+            }
+        }
+    },
 
-	//private
-	addData : function(record) {
-		for(var i = 0; i < this.columns.length; i++) {
-			var c = this.columns[i];
-			this.columnData[c] = this.columnData[c] + record.data[c];
-		}
-	},
+    //private
+    addData : function(record) {
+        for(var i = 0; i < this.columns.length; i++) {
+            var c = this.columns[i];
+            this.columnData[c] = this.columnData[c] + record.data[c];
+        }
+    },
 
-	//private
-	update : function(record) {
-		for(var i = 0; i < this.columns.length; i++) {
-			var c = this.columns[i];
-			if(record.modified[c])
-				this.columnData[c] = this.columnData[c] + record.data[c] - record.modified[c];
-		}
-	},
+    //private
+    update : function(record) {
+        for(var i = 0; i < this.columns.length; i++) {
+            var c = this.columns[i];
+            if(record.modified[c])
+                this.columnData[c] = this.columnData[c] + record.data[c] - record.modified[c];
+        }
+    },
 
-	//private
-	removeData : function(record, index) {
-		for(var i = 0; i < this.columns.length; i++) {
-			var c = this.columns[i];
-			this.columnData[c] = this.columnData[c] - record.data[c];
-		}
-	},
+    //private
+    removeData : function(record, index) {
+        for(var i = 0; i < this.columns.length; i++) {
+            var c = this.columns[i];
+            this.columnData[c] = this.columnData[c] - record.data[c];
+        }
+    },
 
-	//private
-	clear : function() {
-		for(var i = 0; i < this.columns.length; i++) {
-			var c = this.columns[i];
-			this.columnData[c] = 0;
-		}
-	},
+    //private
+    clear : function() {
+        for(var i = 0; i < this.columns.length; i++) {
+            var c = this.columns[i];
+            this.columnData[c] = 0;
+        }
+    },
 
-	/***
-   * As the implementation of pie series is quite different to other series types,
-   * it is not recommended to override this method
-   */
-	getData : function(record, seriesData) {
-		var p=null;
-		// Summed up the category among the series data
-		if(this.totalDataField) {
-			var found = null;
-			for(var i = 0; i < seriesData.length; i++) {
-				if(seriesData[i].name == record.data[this.categorieField]) {
-					found = i;
-					seriesData[i].y += record.data[this.dataField];
-					break;
-				}
-			}
-			if(found === null) {
-				
-				if (this.colorField && record.data[this.colorField]) {
-					p={
-						name: record.data[this.categorieField],
-						y: record.data[this.dataField],
-						color: record.data[this.colorField]
-					};
-				} else {
-					p={
-						name: record.data[this.categorieField],
-						y: record.data[this.dataField]
-					};
-				}
-				if(this.radiusValued===true && this.radiusField){
-				    p.r=record.data[this.radiusField];
-				}
-				seriesData.push(p);
-				i = seriesData.length - 1;
-			}
-			return seriesData[i];
-		}
+    /***
+     * As the implementation of pie series is quite different to other series types,
+     * it is not recommended to override this method
+     */
+    getData : function(record, seriesData) {
 
-		if(this.useTotals) {
-			this.addData(record);
-			return [];
-		}
+        // Summed up the category among the series data
+        if(this.totalDataField) {
+            var found = null;
+            for(var i = 0; i < seriesData.length; i++) {
+                if(seriesData[i].name == record.data[this.categorieField]) {
+                    found = i;
+                    seriesData[i].y += record.data[this.dataField];
+                    break;
+                }
+            }
+            if(found === null) {
+                if (this.colorField && record.data[this.colorField]) {
+                    seriesData.push({
+                        name: record.data[this.categorieField],
+                        y: record.data[this.dataField],
+			r: record.data[this.radiusField],
+                        color: record.data[this.colorField],
+                        record: this.bindRecord ? record : null
+                    });
+                } else {
+                    seriesData.push({
+                        name: record.data[this.categorieField],
+                        y: record.data[this.dataField],
+			r: record.data[this.radiusField],
+                        record: this.bindRecord ? record : null
+                    });
+                }
+                i = seriesData.length - 1;
+            }
+            return seriesData[i];
+        }
 
-		if (this.colorField && record.data[this.colorField]) {
-			p= {
-				name: record.data[this.categorieField],
-				y: record.data[this.dataField],
-				color: record.data[this.colorField]
-			};
-		} else {
-			p = {
-				name: record.data[this.categorieField],
-				y: record.data[this.dataField]
-			};
-		}
-		if(this.radiusValued===true && this.radiusField){
-		    p.r=record.data[this.radiusField];
-		}
-		return p;
-	},
+        if(this.useTotals) {
+            this.addData(record);
+            return [];
+        }
 
-	getTotals : function() {
-		var a = new Array();
-		for(var i = 0; i < this.columns.length; i++) {
-			var c = this.columns[i];
-			a.push([c, this.columnData[c]]);
-		}
-		return a;
-	}
+        if (this.colorField && record.data[this.colorField]) {
+            return {
+                name: record.data[this.categorieField],
+                y: record.data[this.dataField],
+		r: record.data[this.radiusField],
+                color: record.data[this.colorField],
+                record: this.bindRecord ? record : null
+            };
+        } else {
+            return {
+                name: record.data[this.categorieField],
+                y: record.data[this.dataField],
+		r: record.data[this.radiusField],
+                record: this.bindRecord ? record : null
+            };
+        }
+    },
+
+    getTotals : function() {
+        var a = new Array();
+        for(var i = 0; i < this.columns.length; i++) {
+            var c = this.columns[i];
+            a.push([c, this.columnData[c]]);
+        }
+        return a;
+    }
 
 });
+
